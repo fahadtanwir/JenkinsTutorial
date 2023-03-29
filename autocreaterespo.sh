f8ffc762ea8f3928f295cad42a19d32b8be06054
fahadtanwir <fahad.tanwir@hotmail.com>
2023-03-29 05:53:21 +0500

    Update autocreaterespo.sh

--- autocreaterespo.sh
+++ autocreaterespo.sh
@@ -1,16 +1,13 @@
 ACCESS_TOKEN=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
 
-# Read the repository name from the argument passed from the GUI
+
 REPO_NAME=$1
 
-# Create the new repository
 curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/user/repos -d '{"name":"'$REPO_NAME'"}'
 
-# Initialize the local Git repository
 git init
 git add .
 git commit -m "Initial commit"
 
-# Push the local repository to the remote repository on GitHub
 git remote add origin https://github.com/<USERNAME>/$REPO_NAME.git
 git push -u origin master
