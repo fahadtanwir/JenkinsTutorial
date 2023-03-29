a24ff6fd37ab0eed3dc7b96274d17f88375eb7e3
fahadtanwir <fahad.tanwir@hotmail.com>
2023-03-29 06:06:31 +0500

    Update autocreaterespo.sh

--- autocreaterespo.sh
+++ autocreaterespo.sh
@@ -1,13 +1,11 @@
-ACCESS_TOKEN=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
-
-
-REPO_NAME=$1
-
-curl -H "Authorization: token $ACCESS_TOKEN" https://api.github.com/user/repos -d '{"name":"'$REPO_NAME'"}'
-
-git init
-git add .
-git commit -m "Initial commit"
-
-git remote add origin https://github.com/<USERNAME>/$REPO_NAME.git
-git push -u origin master
+stage('Create GitHub Repository') {
+  steps {
+    script {
+      def github = github()
+      def repoName = env.JOB_NAME.replace("/", "-")
+      def ghRepo = github.getRepository("your-github-username/${repoName}")
+      
+      ghRepo.create()
+    }
+  }
+}
